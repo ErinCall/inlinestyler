2dce6b521818ee7e0904fa28c3d46631259b5c8c
Andrew Lorente <andrew.lorente@gmail.com>
2012-02-09 15:49:58 -0800

    Document unimplemented tests.

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -8,35 +8,32 @@ class TestCase(object):
         f = open(test_html_file,'r')
         self.html = f.read()
         f.close()
-    
+
     def test_strip_styles(self):
         styler = InlineStyler(self.html)
         css_list = styler._strip_styles()
         #css_list is a certain length
         assert len(css_list) == 4
-        
+
     def test_style_application(self):
-        assert False
-    
+        assert False, 'not yet implemented'
+
     def test_conversion(self):
-        assert False
-    
+        assert False, 'not yet implemented'
+
     def test_remove_whitepace(self):
         inpt = 'body{\r\ntest}\t '
         string2 = remove_whitepace(inpt)
-        print string2
         assert string2 == 'body{test}'
-        
+
     def test_css_load(self):
         styler = InlineStyler(self.html)
         css_list = styler._strip_styles()
-        print css_list
         string = ''.join(css_list)
-        assert False
-        
+        assert False, 'not yet implemented'
+
     def test_convert(self):
         styler = InlineStyler(self.html)
         new_html = styler.convert()
-        print new_html
         assert new_html
-        assert False
+        assert False, 'not yet implemented'
