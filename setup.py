4e8e7d1c93656925f17608eb14c0f999c9ee8a8b
Andrew Lorente <andrew.lorente@myemma.com>
2012-06-06 11:51:56 -0700

    [#13] Update to beautifulsoup 4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="inlinestyle",
-      version="0.1",
+      version="0.2",
       description="convert html style blocks to inline style statements",
       author="Matt George",
       author_email="mgeorge@gmail.com",
